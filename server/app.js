0c9de06549b18999add48347c28885dc9a1e4332
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2024-11-16 16:40:31 +0530

    Debugging

--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ import profileRouter from './routes/profile.routes.js';
 
 const app = express();
 
-app.use(cors(
-    {
-    origin: ["https://e-scanify-oqnjszl5z-sudhanshu-tiwaris-projects-8f656169.vercel.app"],
-    credentials: true,
-    }
-))
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["https://e-scanify.vercel.app"], 
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/v1/profile", profileRouter)
