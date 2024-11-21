a03f99cdc0632d101ece9dbd99f89ef065d409bb
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2024-11-22 00:19:48 +0530

    Updated link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-import CertificateDetails from "./pages/Details/CertificateDetails";
-import WebCert from "./pages//Certificates/WebCert";
-import CertificatePreview from "./pages/Preview/CertificatePreview";
-import Home from "./pages/Home/home";
+import CertificateDetails from "./pages/Details/CertificateDetails.jsx";
+import WebCert from "./pages//Certificates/WebCert.jsx";
+import CertificatePreview from "./pages/Preview/CertificatePreview.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
   return (
